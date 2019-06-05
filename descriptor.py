5f5e31de0229b879ac89195debee255d5a0b0f68
Yerzhan Serikbay <yerzhan.tulkubayev@gmail.com>
2019-06-05 10:33:08 +0800

    chore: set image search

---
+++ descriptor.py
@@ -0,0 +1,19 @@
+import imutils
+import cv2
+
+
+class RGBHistogram:
+    def __init__(self, bins):
+        self.bins = bins
+
+    def describe(self, image):
+        hist = cv2.calcHist([image], [0, 1, 2],
+                            None, self.bins, [0, 256, 0, 256, 0, 256])
+
+        if imutils.is_cv2():
+            hist = cv2.normalize(hist)
+
+        else:
+            hist = cv2.normalize(hist, hist)
+
+        return hist.flatten()
---
+++ image_search/views.py
@@ -0,0 +1,49 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import numpy as np
+import urllib
+import json
+import cv2
+import os
+
+@csrf_exempt
+def find_image(request):
+    data = {"success": False}
+
+    if request.method == "POST":
+        if request.FILES.get("image", None) is not None:
+            image = _grab_image(stream=request.FILES["image"])
+        else:
+            url = request.POST.get("url", None)
+
+            if url is None:
+                data["error"] = "No URL provided."
+                return JsonResponse(data)
+
+            image = _grab_image(url=url)
+
+        # work with an image
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        data.update({"image_name": True})
+
+    return JsonResponse(data)
+
+
+def _grab_image(path=None, stream=None, url=None):
+    if path is not None:
+        image = cv2.imread(path)
+
+    else:
+        if url is not None:
+            resp = urllib.urlopen(url)
+            data = resp.read()
+
+        elif stream is not None:
+            data = stream.read()
+
+        image = np.asarray(bytearray(data), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    return  image
